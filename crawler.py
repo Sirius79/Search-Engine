3f1c188e6d18bf5113992045a43d20c16e3bd2a0
Rajorshi Chaudhuri <rajorshi.chaudhuri@spit.ac.in>
2018-03-08 13:43:56 +0530

    updated for unique visit

--- crawler.py
+++ crawler.py
@@ -1,7 +1,9 @@
 import re
 import queue
+import os
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
+from bs4 import BeautifulSoup
 
 class Crawler():
   
@@ -9,7 +11,7 @@ class Crawler():
     if links is None:
         links = []
     self.links = links
-    self._old_links = []
+    self.old_links = []
     self.q = queue.Queue()
     if links is not None:
       for link in links:
@@ -20,20 +22,22 @@ class Crawler():
     try:
       html_page = urlopen(req)
       soup = BeautifulSoup(html_page, 'html5lib')
-      for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-        if link not in self._old_links:
-          self.q.put(link.get('href'))
-          print(link.get('href'))
+      for _link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+        if _link not in self.old_links:
+          self.q.put(_link.get('href'))
     except HTTPError as e:
       print('Error code: ', e.code)
     except URLError as e:
       print('Reason: ', e.reason)
     except Exception:
-      print('good!')
+      print('Ignore')
   
   def crawlLinks(self):
     while not self.q.empty():
       link = self.q.get()
-      if link not in self._old_links:
-        self._old_links.append(link)
-      self.getLinks(link)
+      if link not in self.old_links:
+        self.old_links.append(link)
+        self.getLinks(link)
+      else:
+        continue
+      print("Links visted: " + str(len(self.old_links)))
